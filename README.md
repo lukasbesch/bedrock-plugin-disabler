8af84352ae33f575b4e6c5d5ccd2fc93aa7b7dc6
Lukas Besch <connect@lukasbesch.com>
2019-11-26 14:59:25 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+# Disable Plugins
+
+This plugins allows you to define disabled plugins using PHP constants.
+
+
+## Installation
+
+### Composer
+
+`composer require lukasbesch/disable-plugins`
+
+### Manual
+
+Download [the latest release](/lukasbesch/disable-plugins/releases/latest/download/disable-plugins.zip) and place it in your `wp-content/plugins` folder.
+
+
+## Usage
+
+### Standard WordPress
+
+Place this in your `wp-config.php` and add your plugins.
+
+```php
+if (!defined('DISABLED_PLUGINS') {
+    define('DISABLED_PLUGINS', serialize([
+        'autoptimize/autoptimize.php',
+        'updraftplus/updraftplus.php',
+        'wp-super-cache/wp-cache.php',
+        'w3-total-cache/w3-total-cache.php',
+    ]));
+}
+```
+
+### Bedrock
+
+Place this in your `config/environments/development.php ` and add your plugins.
+
+```php
+Config::define('DISABLED_PLUGINS', serialize([
+    'autoptimize/autoptimize.php',
+    'updraftplus/updraftplus.php',
+    'wp-super-cache/wp-cache.php',
+    'w3-total-cache/w3-total-cache.php',
+]));
+```
---
+++ disable-plugins.php
@@ -0,0 +1,47 @@
+<?php
+/*
+Plugin Name:  Disable plugins
+Plugin URI:   https://lukasbesch.com/
+Description:  Disable certain plugins in non-production environments using PHP constants.
+Version:      1.0.0
+Author:       Lukas Besch
+Author URI:   https://lukasbesch.com/
+License:      MIT License
+*/
+
+if ( defined('DISABLED_PLUGINS') && ! empty( DISABLED_PLUGINS ) ) {
+
+    function get_disabled_plugins()
+    {
+        $plugins_to_disable = unserialize(DISABLED_PLUGINS, [false]);
+        return (!empty($plugins_to_disable) && is_array($plugins_to_disable)) ? $plugins_to_disable : [];
+    }
+
+    function show_plugin_not_activatable_notice() {
+        global $wp_list_table;
+        $plugins_to_disable = get_disabled_plugins();
+        foreach ($wp_list_table->items as $key => $val) {
+            if ( in_array($key, $plugins_to_disable, true) ) {
+                $item = $wp_list_table->items[$key];
+                $item['Name'] = '[Disabled] ' . $item['Name'];
+                $item['Description'] .= '<br><strong style="color:red">Disabled in this environment.</strong>';
+                unset( $wp_list_table->items[$key] );
+                $wp_list_table->items[$key] = $item;
+            }
+        }
+    }
+
+    $plugins_to_disable = get_disabled_plugins();
+
+    if (!empty($plugins_to_disable) && is_array($plugins_to_disable)) {
+        require_once(__DIR__ . '/disable-plugins-class.php');
+        $utility = new DisablePlugins($plugins_to_disable);
+
+        // part below is optional but for me it is crucial
+        // error_log('Locally disabled plugins: ' . var_export($plugins_to_disable, true));
+    }
+
+    add_action('pre_current_active_plugins', 'show_plugin_not_activatable_notice');
+
+}
+
